f21f705ea6215ee39a94d001ff17420bf84ebbd4
asiuzzaman <zaman1304006@gmail.com>
2024-10-06 13:37:52 +0600

    Implement trie and experiments few cases

--- PlayCPPWithXcode/PlayCPPWithXcode/main.cpp
+++ PlayCPPWithXcode/PlayCPPWithXcode/main.cpp
@@ -1,14 +1,88 @@
-//
-//  main.cpp
-//  PlayCPPWithXcode
-//
-//  Created by Md. Asiuzzaman on 6/10/24.
-//
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
 
-#include <iostream>
+#define MX 26
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+ struct TrieNode {
+     bool isEnd;
+     int count;
+     TrieNode * child[MX];
+
+     TrieNode() {
+         for(int i=0; i<MX; i++) {
+             child[i] = NULL;
+         }
+         isEnd = false;
+         count = 0;
+     }
+ };
+
+ void insertKey(TrieNode * root, const string& key) {
+     TrieNode * cur = root;
+
+     for(auto ch: key) {
+         int idx = ch - 'a';
+
+         if(cur->child[idx] == NULL) {
+             cur->child[idx] = new TrieNode();
+         }
+         cur = cur->child[idx];
+         cur->count += 1;  // age or pore te kono problem hoy kina
+     }
+     cur->isEnd = true;
+ }
+
+ int findIndex(TrieNode * root, const string& key) {
+
+     TrieNode * cur = root;
+
+     int index = 0;
+
+     for(auto ch: key) {
+
+         int idx = ch - 'a';
+
+         for(int i=0; i<idx; i++) {
+             if (cur->child[i] != NULL) {
+                 index += cur->child[i]->count;
+             }
+         }
+         if (cur->child[idx] != NULL) {
+             cur = cur->child[idx];
+         } else {
+             return -1; // word not found
+         }
+     }
+
+     if (cur->isEnd) return index;
+
+     return -1;
+ }
+
+int main() {
+
+    TrieNode * root = new TrieNode();
+
+    //insertKey(root, "asdb");
+    vector<string> arr = {"aaa", "bcd", "aac", "sam", "ppy", "ppa", "pqs" ,"yound", "ggdks"};
+     for(auto str: arr) {
+         insertKey(root, str);
+     }
+
+
+
+    printf("the index is: %d\n", findIndex(root, "pqs"));
+
+//    for(auto str: keys) {
+//        cout<<"Key: "<<str<<"->";
+//
+//        if (searchKey(root, str)) {
+//            cout<<"Present"<<endl;
+//        }
+//        else {
+//            cout<<"Absent"<<endl;
+//        }
+//    }
 }
